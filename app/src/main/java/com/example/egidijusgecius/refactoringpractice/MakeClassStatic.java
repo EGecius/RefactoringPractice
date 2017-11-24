87b54f907e88711d0c5883258dba020b83de0e7b
update gradle <egidijusgecius@Egidijuss-MBP.default>
2017-11-24 20:01:09 +0000

    MakeClassStatic

--- app/src/main/java/com/example/egidijusgecius/refactoringpractice/MakeClassStatic.java
+++ app/src/main/java/com/example/egidijusgecius/refactoringpractice/MakeClassStatic.java
@@ -2,5 +2,13 @@ package com.example.egidijusgecius.refactoringpractice;
 
 public class MakeClassStatic {
 
-    // TODO: 24/11/2017 finished here
+    private final String outerClassId = "innerClassId";
+
+    class OuterClass {
+
+        private final String innerClassId = "inner_class_id";
+        private final String externalId = outerClassId;
+
+    }
+
 }
