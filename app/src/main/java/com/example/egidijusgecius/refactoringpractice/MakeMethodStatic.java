0836287333aba2bd81c834b1e797ae6ede685a37
update gradle <egidijusgecius@Egidijuss-MBP.default>
2017-11-24 20:08:03 +0000

    MakeMethodStatic

---
+++ app/src/main/java/com/example/egidijusgecius/refactoringpractice/MakeMethodStatic.java
@@ -0,0 +1,12 @@
+package com.example.egidijusgecius.refactoringpractice;
+
+public class MakeMethodStatic {
+
+    public int i;
+    public int j;
+
+    public int getConnection() {
+
+        return i + j;
+    }
+}
