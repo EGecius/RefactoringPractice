5bd9ee933626f2fe03a0628bdea6e0c94240548f
update gradle <>
2017-11-27 20:05:40 +0000

    move MainActivity to a separate package

--- app/src/main/java/andoid/MainActivity.java
+++ app/src/main/java/andoid/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.egidijusgecius.refactoringpractice;
+package andoid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.egidijusgecius.refactoringpractice.R;
 
 public class MainActivity extends AppCompatActivity {
 
