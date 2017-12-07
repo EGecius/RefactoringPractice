9b2117af0e8bbd17fda189316d771f8306d20096
Egidijus Gecius <e.gecius@gmail.com>
2017-12-07 21:26:16 +0000

    add classes for live templates practice

---
+++ app/src/main/java/live_templates_practice/GenerateViewConstructors.java
@@ -0,0 +1,22 @@
+package live_templates_practice;
+
+import android.content.Context;
+import android.view.View;
+
+public class GenerateViewConstructors extends View {
+
+
+    public GenerateViewConstructors(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+
+        // find view by it with cast
+
+
+    }
+
+
+}
---
+++ app/src/main/java/live_templates_practice/LIveTemplatesPractice.java
@@ -0,0 +1,27 @@
+package live_templates_practice;
+
+import android.content.Context;
+
+public class LIveTemplatesPractice {
+
+    String field1;
+    //  constant
+    Integer field2;
+    //  constant - key
+    Context context;
+
+
+    // Toast - make & show
+    void showToast() {
+
+    }
+
+    // add fix me comment
+
+
+    // add no operation comment to method
+    void emptyMehtod() {
+
+    }
+
+}
